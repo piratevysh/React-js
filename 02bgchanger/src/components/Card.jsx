4a386e85720776990800d7cbdcb14bf98abdc7de
Vyshnav <vyshnav@Vyshnavs-MacBook-Air.local>
2024-03-16 18:54:31 +0530

    Currency convertor  project

--- 02bgchanger/src/components/Card.jsx
+++ 02bgchanger/src/components/Card.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function card(props) {
     
   return (
+    
     <div>
         <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
         <img class="w-24 h-24 rounded-full mx-auto" src="/vyshnav.png" alt="" width="384" height="512" />
@@ -10,7 +11,7 @@ function card(props) {
     <blockquote>
       <p class="text-lg">
         “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
+        on large teams. It's easy to customize, adapts to any design,
         and the build size is tiny.”
       </p>
     </blockquote>
